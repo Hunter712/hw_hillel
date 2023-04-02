2a6e6c8f1dc64cdcddb110254720a476933b92cc
Vlad <seven712234234@gmail.com>
2023-04-02 15:16:54 +0300

    added 2 print functions

--- HW4.py
+++ HW4.py
@@ -1,6 +1,6 @@
 number1 = int(input("Enter first number:"))
 number2 = int(input("Enter second number:"))
 
-print('{0} + {1} = {2} \n{3} * {4} = {5}'
-      .format(number1, number2, number1 + number2, number1, number2, number1 * number2))
+print('{0} + {1} = {2}'.format(number1, number2, number1 + number2))
+print('{0} * {1} = {2}'.format(number1, number2, number1 * number2))
 
