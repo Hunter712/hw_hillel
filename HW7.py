b4996ce4f35cf0c117e5017cf4d5f8f88045cee5
Vlad <seven712234234@gmail.com>
2023-04-04 23:15:07 +0300

    added home work 7

---
+++ HW7.py
@@ -0,0 +1,9 @@
+triangle_size = int(input("Enter size of triangle:"))
+
+for i in range(triangle_size):                      #формирую строку
+    fill_triangle = ""
+    for j in range(triangle_size - i - 1):                  #заполняю строку пустым символом пока не дойду до побочной диагонали матрицы,
+        fill_triangle += "1"                                #-1 помогает избежать пустой строки когда i=0 на первой итерации
+    for k in range(triangle_size - i - 1, triangle_size):   #заполняю строку * после достижения побочной диагонали матрицы
+        fill_triangle += "*"
+    print(fill_triangle)
