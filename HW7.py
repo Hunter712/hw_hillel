d3990f9a04258d8a907901c2cdb5432440e3b791
Vlad <seven712234234@gmail.com>
2023-04-04 23:16:19 +0300

    added empty string instead of 1

--- HW7.py
+++ HW7.py
@@ -3,7 +3,7 @@ triangle_size = int(input("Enter size of triangle:"))
 for i in range(triangle_size):                      #формирую строку
     fill_triangle = ""
     for j in range(triangle_size - i - 1):                  #заполняю строку пустым символом пока не дойду до побочной диагонали матрицы,
-        fill_triangle += "1"                                #-1 помогает избежать пустой строки когда i=0 на первой итерации
+        fill_triangle += " "                                #-1 помогает избежать пустой строки когда i=0 на первой итерации
     for k in range(triangle_size - i - 1, triangle_size):   #заполняю строку * после достижения побочной диагонали матрицы
         fill_triangle += "*"
     print(fill_triangle)
