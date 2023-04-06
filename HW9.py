a14e369211fdc325b58a2a409cb23196df4bdb49
Vlad <seven712234234@gmail.com>
2023-04-06 10:32:48 +0300

    deleted extra space at the end of the each string

--- HW9.py
+++ HW9.py
@@ -1,11 +1,14 @@
 n = int(input("Enter n:"))
 
-
+add_extra_space = " "
 for k in range(1, n + 1):                #формирую столбцы в количестве n
     line = (n - k) * "  "                 #формирую пустые символы в начале строки на убывание, для формирования трехугольника
     for i in range(1, k):                #формирую строку от 1 до n
         line += str(i) + " "
 
     for j in range(k, 0, -1):            #формирую строку от n до 1
-        line += str(j) + " "
+        if j == 1:                       #в конце каждой строки убираю спейс
+            line += str(j)
+        else:
+            line += str(j) + " "
     print(line)
