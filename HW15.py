5516f84ed1e429ca76885eddd6d0e397b978573e
Vlad <seven712234234@gmail.com>
2023-04-11 20:38:30 +0300

    added condition to output right values if list not in range

--- HW15.py
+++ HW15.py
@@ -1,5 +1,5 @@
-lst = [11, 12, 13, 16, 17, 18, 19, 13]
-MIN_NUMBER = 13
+lst = [11, 12, 13, 16, 17, 18, 19]
+MIN_NUMBER = 14
 MAX_NUMBER = 15
 
 print(lst)
@@ -7,14 +7,13 @@ print(lst)
 sum_of_numbers = 0
 product_of_numbers = 1
 
-if MAX_NUMBER in lst:
-    for i in lst:
-        if MIN_NUMBER < i <= MAX_NUMBER:
-            sum_of_numbers += i
-            product_of_numbers *= i
-else:
-    sum_of_numbers = 0
-    product_of_numbers = 0
+for i in lst:
+    if MIN_NUMBER < i <= MAX_NUMBER:
+        sum_of_numbers += i
+        product_of_numbers *= i
 
-print(f"sum = {sum_of_numbers} product = {product_of_numbers}")
+if sum_of_numbers != 0:
+    print(f"sum = {sum_of_numbers} product = {product_of_numbers}")
+else:
+    print("sum = 0 product = 0")
 
