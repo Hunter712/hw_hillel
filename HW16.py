92c2c4b54681b05f2256823867d5899649f50a90
Vlad <seven712234234@gmail.com>
2023-04-11 21:03:38 +0300

    fixed index in HW16

--- HW16.py
+++ HW16.py
@@ -1,6 +1,6 @@
 lst = [3.5, 2, 4, 6.2, 8]
 
-for index in range(len(lst) - 1, 1, -1):
+for index in range(len(lst) - 1, 0, -1):
     average_number = (lst[index] + lst[index - 1]) / 2
     lst.insert(index, average_number)
 
