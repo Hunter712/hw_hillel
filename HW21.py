7f8eb0e4f9a2b3e70d2681aa743b671bfc563a9c
Vlad <seven712234234@gmail.com>
2023-04-17 20:12:01 +0300

    fixed writing data to json and other small things in HW21

--- HW21.py
+++ HW21.py
@@ -1,4 +1,4 @@
-string_with_data_in_file = ""
+import json
 
 with open("users.txt", "r") as file:
     string_with_data_in_file = file.read()
@@ -6,11 +6,11 @@ with open("users.txt", "r") as file:
 list_with_data_in_file = string_with_data_in_file.split("\n")  # разбил каждую новую строку файла по элементам списка
 list_with_all_data = []
 
-for i in list_with_data_in_file:
-    sub_list_with_data_in_file = i.split(";")  # формирую подсписок что бы вытянуть отдельно имя, возраст и телефон
+for line in list_with_data_in_file:
+    sub_list_with_data_in_file = line.split(";")  # формирую подсписок что бы вытянуть отдельно имя, возраст и телефон
     dict_with_parced_data = {}
 
-    if "" != sub_list_with_data_in_file[0]:  # проверяю есть ли имя в строке, так же помогает избежать index out of range ексепшина
+    if "" != line and "" != sub_list_with_data_in_file[0]:  # проверяю на наличие строки впринципе, а так же на наличие имени(если имя не указывал но остальные данные были то пропускало)
         dict_with_parced_data["name"] = sub_list_with_data_in_file[0].strip()     # формирую словарь с именем
 
         if sub_list_with_data_in_file[1].isdigit():
@@ -20,15 +20,15 @@ for i in list_with_data_in_file:
 
         if "" != sub_list_with_data_in_file[2]:
             stripped_phones = []
-            for i in sub_list_with_data_in_file[2].split(","):  # формирую словарь со списком номеров и заодно убераю пробелы по бокам
-                stripped_phones.append(i.strip())
+            for phone_number in sub_list_with_data_in_file[2].split(","):  # формирую словарь со списком номеров и заодно убираю пробелы по бокам
+                stripped_phones.append(phone_number.strip())
             dict_with_parced_data["phones"] = stripped_phones
         else:
             dict_with_parced_data["phones"] = []
         list_with_all_data.append(dict_with_parced_data)
 
 with open("users_out.json", "w") as file:
-    file.write(str(list_with_all_data))
+    json.dump(list_with_all_data, file)
 
 output_str = ""
 for sub_dict in list_with_all_data:
