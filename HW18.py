9bf1570725cab86c61c518cfbca799ba399e1492
Vlad <seven712234234@gmail.com>
2023-04-17 20:47:43 +0300

    changed loops and structure for testers data in HW18

--- HW18.py
+++ HW18.py
@@ -1,43 +1,39 @@
 testers_data = {
-    1: {"Alex": ["write test design", "write test script", "review test script", "in office"]},
-    2: {"Petro": ["write test script", "review test script", "not in office"]},
-    3: {"Grisha": ["review test script", "not in office"]},
-    4: {"Ivan": ["review test script", "in office"]},
-    5: {"Goga": ["write test script", "in office"]},
-    6: {"Zhora": ["write test script", "review test script", "in office"]},
+    1: {"in office": ["write test design", "write test script", "review test script"]},
+    2: {"not in office": ["write test script", "review test script"]},
+    3: {"not in office": ["review test script"]},
+    4: {"in office": ["review test script"]},
+    5: {"in office": ["write test script"]},
+    6: {"in office": ["write test script", "review test script"]},
 }
 
-list_with_testers_id = []
 who_can_write_only_scripts = []
 who_is_in_office_today = []
 who_can_write_review_scripts_today = []
 
-for x in testers_data:
-    list_with_testers_id.append(x)
-
-list_with_testers_id.sort()
+list_with_testers_id = sorted(list(testers_data.keys()))
 print("All testers in a team " + str(list_with_testers_id))
 
-for i in testers_data:
-    for j in testers_data[i]:
-        if "write test script" in testers_data[i][j] and "write test design" not in testers_data[i][j]:
-            who_can_write_only_scripts.append(i)
+for user_id, person_data in testers_data.items():
+    for list_with_skills in person_data.values():
+        if "write test script" in list_with_skills and "write test design" not in list_with_skills:
+            who_can_write_only_scripts.append(user_id)
 
 who_can_write_only_scripts.sort()
 print("Testers who can write only scripts in a team " + str(who_can_write_only_scripts))
 
-for i in testers_data:
-    for j in testers_data[i]:
-        if "in office" in testers_data[i][j]:
-            who_is_in_office_today.append(i)
+for user_id, person_data in testers_data.items():
+    for list_with_skills in person_data.keys():
+        if "in office" == list_with_skills:
+            who_is_in_office_today.append(user_id)
 
 who_is_in_office_today.sort()
 print("Testers who is in office today " + str(who_is_in_office_today))
 
-for i in testers_data:
-    for j in testers_data[i]:
-        if "write test script" in testers_data[i][j] and "review test script" in testers_data[i][j] and "in office" in testers_data[i][j]:
-            who_can_write_review_scripts_today.append(i)
+for user_id, person_data in testers_data.items():
+    for is_here, list_with_skills in person_data.items():
+        if "write test script" in list_with_skills and "review test script" in list_with_skills and "in office" == is_here:
+            who_can_write_review_scripts_today.append(user_id)
 
 who_can_write_review_scripts_today.sort()
 print("Testers who is in office today and can write and review scripts" + str(who_can_write_review_scripts_today))
