9ab22ba25ebb58f81dbb6d5d98f90003bf6502f7
Vlad <seven712234234@gmail.com>
2023-04-29 18:37:31 +0300

    fixed output info to file

--- HW25.py
+++ HW25.py
@@ -1,24 +1,34 @@
 def call_counter(file_name):
     def read_number():
         with open(file_name, "r") as file:
-            call_number = file.read()
-            if call_number == "":
-                return 0
-            else:
-                return int(call_number)
+            file_str = file.read()
+        return file_str
 
-    def write_number(call_number):
+    def write_number(file_str):
         with open(file_name, "w") as file:
-            file.write(str(call_number))
+            file.write(file_str)
 
     def inner(function):
-        def wrapper(a, b):
-            call_add_number = read_number()
-            function(a, b)
-            call_add_number += 1
-            result = f"Function 'add' was called {call_add_number} times"
-            write_number(call_add_number)
-            return result
+        def wrapper(*args, **kwargs):
+            if read_number():  # проверяю на пустой файл
+                file_list = read_number().split("\n")
+                find_unwritten_function = 0
+                for line in range(len(file_list)):  # прохожу по каждой строке файла
+                    if function.__name__ in file_list[line]:  # проверяю есть ли в строке текущая функция
+                        file_str = file_list[line].split(" ")  # формирую новый лист из строки, нахожу число,
+                        for i in range(len(file_str)):  # перезаписываю на одно больше и кладу обратно в файл
+                            if file_str[i].isdigit():
+                                file_str[i] = str(int(file_str[i]) + 1)
+                        file_list[line] = ' '.join(file_str)
+
+                if read_number().count(function.__name__) == 0:
+                    file_list.append(
+                        f"Function {function.__name__} was called 1 times")  # если функция ранее не записывалась в файл то записываю ее
+                write_number('\n'.join(file_list))
+            else:  # если файл пустой записываю функцию
+                write_number(f"Function {function.__name__} was called 1 times")
+            return function(*args, **kwargs)
+
         return wrapper
 
     return inner
@@ -29,6 +39,10 @@ def add(a, b):
     return a + b
 
 
-print(add(4, 6))
-print(add(4, 6))
-print(add(4, 6))
+@call_counter('data.txt')
+def odd(a, b, c):
+    return a + b + c
+
+
+add(3, 4)
+odd(3, 4, 5)
