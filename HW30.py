663e0acfabf55f56358e5e622a0ee58fa5f3ed4d
Vlad <seven712234234@gmail.com>
2023-04-29 17:17:25 +0300

    added right aligned

--- HW30.py
+++ HW30.py
@@ -12,7 +12,7 @@ for d in rrule(DAILY, dtstart=start_date, until=end_date):
     if lecture_numbers < 32:
         if d.strftime("%A") == "Monday" or d.strftime("%A") == "Thursday":
             lecture_numbers += 1
-            lecture_dates.append(f"Lecture  {lecture_numbers}: {d.strftime('%d %b %Y')} 19:15")
+            lecture_dates.append(f"Lecture  {lecture_numbers:>2}: {d.strftime('%d %b %Y')} 19:15")
 
 for i in lecture_dates:
     print(i)
