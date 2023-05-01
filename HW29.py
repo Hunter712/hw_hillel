88c8560e0637ba20eafadc6f270a3d7db99d674d
Vlad <seven712234234@gmail.com>
2023-05-01 16:03:57 +0300

    added comments to HW29

--- HW29.py
+++ HW29.py
@@ -1,11 +1,11 @@
 def linearize_list(lst):
     if lst:
-        if isinstance(lst[0], list):
-            linearize_list(lst[0])
-            return linearize_list(lst[1:])
-        elif isinstance(lst[0], int):
-            new_lst.append(lst[0])
-            return linearize_list(lst[1:])
+        if isinstance(lst[0], list):    # проверяю текущий элемент на список
+            linearize_list(lst[0])      # если попался список передаю его еще раз что бы углубиться на уровень
+            return linearize_list(lst[1:])  # передаю остаток списка после текщуго списка
+        elif isinstance(lst[0], int):   # проверяю текущий элемент на число
+            new_lst.append(lst[0])      # если число то добавляю его в новую строку
+            return linearize_list(lst[1:])  # передаю остаток списка после числа
 
 
 lst = [1, 2, [3, 4, [5, 6], 7], 8, [9, [10]], 11]
