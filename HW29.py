a883a25b5e3c62ca237215a1581ef85fe367f9a0
Vlad <seven712234234@gmail.com>
2023-05-01 17:43:22 +0300

    fixed float numbers in HW29

--- HW29.py
+++ HW29.py
@@ -3,12 +3,12 @@ def linearize_list(lst):
         if isinstance(lst[0], list):    # проверяю текущий элемент на список
             linearize_list(lst[0])      # если попался список передаю его еще раз что бы углубиться на уровень
             return linearize_list(lst[1:])  # передаю остаток списка после текщуго списка
-        elif isinstance(lst[0], int):   # проверяю текущий элемент на число
+        elif isinstance(lst[0], int) or isinstance(lst[0], float):   # проверяю текущий элемент на число
             new_lst.append(lst[0])      # если число то добавляю его в новую строку
             return linearize_list(lst[1:])  # передаю остаток списка после числа
 
 
-lst = [1, 2, [3, 4, [5, 6], 7], 8, [9, [10]], 11]
+lst = [1, 2.2, [3, 4, [5.0, 6], 7], 8, [9.6, [10]], 11]
 new_lst = []
 linearize_list(lst)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 print(new_lst)
