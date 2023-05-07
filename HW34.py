186e3b221f21b5e880fe94b3feb51241544c8ffd
Vlad <seven712234234@gmail.com>
2023-05-07 18:55:56 +0300

    added doc strings to HW34

--- HW34.py
+++ HW34.py
@@ -12,8 +12,9 @@ class Animal:
         self.is_pet = is_pet
         self.good_month_analyses = good_month_analyses
 
-    """Предуагадываю сколько теоретически осталось жить животному"""
     def calculate_animal_age_predict(self):
+        """ Предуагадываю сколько теоретически осталось жить животному"""
+
         if self.breed_of_animal in self.animals:
             if self.is_pet and self.good_month_analyses:
                 return self.animals.get(self.breed_of_animal) - self.age
@@ -22,8 +23,8 @@ class Animal:
         else:
             return random.randint(3, 15)
 
-    """Рассчитываю в каком месяце лучше сводить животных в зависимости от породы"""
     def calculate_best_month_to_meet_couple(self):
+        """Рассчитываю в каком месяце лучше сводить животных в зависимости от породы"""
         if self.breed_of_animal == "Cat":
             return random.randint(3, 5)
         elif self.breed_of_animal == "Dog":
@@ -39,20 +40,20 @@ class FindPerfectCouple:
         self.animal1 = animal1
         self.animal2 = animal2
 
-    """Ищу пару для животного в зависимости от того совпадают у них месяцы или нет"""
     def find_couple(self):
+        """Ищу пару для животного в зависимости от того совпадают у них месяцы или нет"""
         if self.animal1.calculate_best_month_to_meet_couple() == self.animal2.calculate_best_month_to_meet_couple():
             return f"{self.animal1.name} and {self.animal2.name} is perfect couple"
         else:
             return "Bad couple"
 
-    """Предискиваю лучший зоо магазин"""
     def best_shop_for_animal(self):
+        """Предискиваю лучший зоо магазин"""
         return max(self.best_shops, key=self.best_shops.get)
 
-    """Генератор случайных имен для животного"""
     @staticmethod
     def generate_name_for_baby_animal():
+        """Генератор случайных имен для животного"""
         return names.get_first_name()
 
 
