f72408d8c7c1293cb161867b1e1919abdd45a45a
Vlad <seven712234234@gmail.com>
2023-05-07 18:47:52 +0300

    added doc strings to HW34

--- HW34.py
+++ HW34.py
@@ -12,6 +12,7 @@ class Animal:
         self.is_pet = is_pet
         self.good_month_analyses = good_month_analyses
 
+    """Предуагадываю сколько теоретически осталось жить животному"""
     def calculate_animal_age_predict(self):
         if self.breed_of_animal in self.animals:
             if self.is_pet and self.good_month_analyses:
@@ -21,6 +22,7 @@ class Animal:
         else:
             return random.randint(3, 15)
 
+    """Рассчитываю в каком месяце лучше сводить животных в зависимости от породы"""
     def calculate_best_month_to_meet_couple(self):
         if self.breed_of_animal == "Cat":
             return random.randint(3, 5)
@@ -37,15 +39,18 @@ class FindPerfectCouple:
         self.animal1 = animal1
         self.animal2 = animal2
 
+    """Ищу пару для животного в зависимости от того совпадают у них месяцы или нет"""
     def find_couple(self):
         if self.animal1.calculate_best_month_to_meet_couple() == self.animal2.calculate_best_month_to_meet_couple():
             return f"{self.animal1.name} and {self.animal2.name} is perfect couple"
         else:
             return "Bad couple"
 
+    """Предискиваю лучший зоо магазин"""
     def best_shop_for_animal(self):
         return max(self.best_shops, key=self.best_shops.get)
 
+    """Генератор случайных имен для животного"""
     @staticmethod
     def generate_name_for_baby_animal():
         return names.get_first_name()
