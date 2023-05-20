e626a58631b7efcd85c26580879d6a01e5989b19
Vlad <seven712234234@gmail.com>
2023-05-20 14:58:47 +0300

    fixed printing fields for few inherited classes

--- HW37.py
+++ HW37.py
@@ -1,16 +1,14 @@
 class AttributePrinterMixin:
-    def get_class_atributes(self):
-        class_name = self.__class__.__name__
-        dict_with_atrs = self.__dict__
-        resulted_str = f"{class_name}: \u007b\n"
+    def __str__(self):
+        resulted_str = f"{self.__class__.__name__}: \u007b\n"
 
         # формирую список, каждый элемент которого будет сожержать имя поля и его значение в одной строке
-        result_list = [str(k) + ": " + str(v) for k, v in dict_with_atrs.items()]
+        result_list = [str(k) + ": " + str(v) for k, v in self.__dict__.items()]
 
         # вырезаю символы связанные с доступами к полям, из элементов списка
         for index in range(len(result_list)):
             if result_list[index][0] == "_":
-                if result_list[index][1] == class_name:
+                if result_list[index][2] == "_":
                     result_list[index] = result_list[index][4:]
                 else:
                     result_list[index] = result_list[index][1:]
@@ -21,8 +19,15 @@ class AttributePrinterMixin:
         return resulted_str + "\u007d"
 
 
-class A(AttributePrinterMixin):
+class B:
     def __init__(self):
+        self.__private_in_B = 'private_in_B'
+        self._private1_in_B = 'private1_in_B'
+
+
+class A(B, AttributePrinterMixin):
+    def __init__(self):
+        B.__init__(self)
         self.public_filed = 3
         self._protected_field = 'q'
         self.__private_field = [1, 2, 3]
@@ -32,4 +37,4 @@ class A(AttributePrinterMixin):
 
 
 a = A()
-print(a.get_class_atributes())
+print(a)
