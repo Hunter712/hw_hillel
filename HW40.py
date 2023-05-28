5afe938119be83a41c4c85a676f5ef2628a9d412
Vlad <seven712234234@gmail.com>
2023-05-28 12:39:56 +0300

    fixed logic in  HW40

--- HW40.py
+++ HW40.py
@@ -1,26 +1,39 @@
+
 def cash(function):
-    def wrapper(a, b):
-        if wrapper.arg_1 is None and wrapper.arg_2 is None:
-            # сохраняем аргументы первого выполнения функции
-            # и результат выполнения функции
-            wrapper.arg_1 = a
-            wrapper.arg_2 = b
+    # сравниваю аргументы первой выполненной функции и текущей, не зависимо от их позиции
+    def compare_elements(arg1, arg2):
+        new_list = []
+        for element in arg1:
+            if element in arg2:
+                new_list.append(element)
 
-            wrapper.result = function(a, b)
-            return wrapper.result
-        elif wrapper.arg_1 == a and wrapper.arg_2 == b:
-            # если аргументы совпадают то возвращаю сохраненный результат
-            return wrapper.result
-        elif wrapper.arg_1 == b and wrapper.arg_2 == a:
-            # если аргументы совпадают то возвращаю сохраненный результат
-            return wrapper.result
+        if len(new_list) == len(arg1):
+            return True
         else:
-            # если аргументы НЕ совпадают то вычисляю функцию
-            return function(a, b)
+            return False
 
-    wrapper.arg_1 = None
-    wrapper.arg_2 = None
-    wrapper.result = None
+    def wrapper(*args, **kwargs):
+
+        if wrapper.func_data == {} or function.__name__ not in wrapper.func_data:
+            # сохраняю аргументы первого выполнения функции
+            # и результат выполнения функции
+            func_result = function(*args, **kwargs)
+            wrapper.func_data[function.__name__] = list(args)
+            wrapper.func_result[function.__name__] = func_result
+
+            return func_result
+
+        elif function.__name__ in wrapper.func_data:
+            # проверяю совпадает ли функция и ее аргументы
+            if compare_elements(wrapper.func_data[function.__name__], args):
+                # если аргументы совпадают то возвращаю сохраненный результат
+                return wrapper.func_result[function.__name__]
+            else:
+                # если не совпадают то выполняю функцию
+                return function(*args, **kwargs)
+
+    wrapper.func_data = {}
+    wrapper.func_result = {}
     return wrapper
 
 
@@ -29,8 +42,24 @@ def sum_numbers(a, b):
     return a + b
 
 
-print(sum_numbers(2, 3))
-print(sum_numbers(1, 3))
-print(sum_numbers(2, 3))
-print(sum_numbers(3, 2))
-print(sum_numbers(22, 3))
+@cash
+def sum_symbols(a, b, c):
+    return a + b + c
+
+
+@cash
+def sum_lists(a, b):
+    return a + b
+
+
+print("result = ", sum_numbers(2, 3))
+print("result = ", sum_numbers(3, 2))
+print("result = ", sum_numbers(3, 1))
+
+print("result = ", sum_symbols("a", "b", "c"))
+print("result = ", sum_symbols("a", "b", "s"))
+print("result = ", sum_symbols("c", "b", "a"))
+
+print("result = ", sum_lists([1, 2, 3], [4, 5, 6]))
+print("result = ", sum_lists([1, 2, 2], [4, 1, 6]))
+print("result = ", sum_lists([4, 5, 6], [1, 2, 3]))
