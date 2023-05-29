d317f54ac05366e37c58142859e028bad2b91c31
Vlad <seven712234234@gmail.com>
2023-05-29 04:12:05 +0300

    fixed logic in  HW40

--- HW40.py
+++ HW40.py
@@ -3,53 +3,50 @@ def cash(function):
     KWARGS = 1
     FUNCCTION_RESULT = 2
 
-    # сравниваю аргументы первой выполненной функции и текущей, не зависимо от их позиции
-    def compare_elements(arg1, arg2, arg3):
-        new_list = []
-        for element in arg1[ARGS]:
-            if element in arg2:
-                new_list.append(element)
-
-        if arg1[KWARGS] == {}:
-            if len(new_list) == len(arg1[ARGS]):
-                return True
-            else:
-                return False
+    def compare_elements(cash_list, args, kwargs):
+        # проверяю есть ли аргументы в кеше, если нет то возвращаю None что бы первые данные записались в кеш
+        if cash_list:
+            result = None
+            # пробегаюсь по списку закешированных списков аргументов
+            for list_with_args in cash_list:
+                # беру текущий список, достаю из него закешированные args и сравниваю с новыми args
+                # формирую новый список из совпавших елементов из args
+                compared_list = [x for x in list_with_args[ARGS] if x in args]
+                # сравнниваю по длинне список compared_list с текущим закешированным списком,
+                # если совпадают по длинне, значит args идентичны
+                # после чего сравниваю kwargs
+                # если args и kwargs совпадают, присваиваю result закешированный список и возвращаю его
+                # если не совпадают то иду дальше по списку закешированных списков
+                if len(compared_list) == len(list_with_args[ARGS]) and list_with_args[KWARGS] == kwargs:
+                    result = list_with_args
+            return result
         else:
-            if len(new_list) == len(arg1[ARGS]) and arg1[KWARGS] == arg3:
-                return True
-            else:
-                return False
+            return None
 
     def wrapper(*args, **kwargs):
-        if wrapper.func_data == {}:
-            # сохраняю аргументы первого выполнения функции
-            # и результат выполнения функции
+        # проверяю совпадают ли *args, **kwargs с закешированными аргументами
+        compare = compare_elements(wrapper.func_data, args, kwargs)
+        # если аргументы совпадают то возвращаю закешированный результат выполнения функции и проваливаюсь в if
+        if compare is not None:
+            # возвращаю закешированный результат выполненния функции
+            return compare[FUNCCTION_RESULT]
+        else:
+            # если аргументы не совпадают то выполняю функцию и добавляю новые данные в кеш
             func_result = function(*args, **kwargs)
-            wrapper.func_data[function.__name__] = [list(args), kwargs, func_result]
-
+            # записываю в кеш список с тремя елементами:
+            # elem1 - список args
+            # elem2 - список kwargs
+            # elem3 - результат выполненния функции
+            wrapper.func_data.append([list(args), kwargs, func_result])
             return func_result
-        elif function.__name__ in wrapper.func_data:
-            # проверяю совпадает ли функция и ее аргументы
-            if compare_elements(wrapper.func_data[function.__name__], args, kwargs):
-                # если аргументы совпадают то возвращаю сохраненный результат
-                return wrapper.func_data[function.__name__][FUNCCTION_RESULT]
-            else:
-                # если не совпадают то выполняю функцию
-                return function(*args, **kwargs)
 
-    wrapper.func_data = {}
+    wrapper.func_data = []
     return wrapper
 
 
 @cash
 def sum_numbers(a, b):
     return a + b
-
-
-@cash
-def sum_symbols(a, b, c):
-    return a + b + c
 
 
 @cash
@@ -60,11 +57,7 @@ def sum_lists(a, b):
 print("result = ", sum_numbers(2, 3))
 print("result = ", sum_numbers(3, 2))
 print("result = ", sum_numbers(3, 1))
-
-
-print("result = ", sum_symbols("a", "b", "c"))
-print("result = ", sum_symbols("a", "b", "s"))
-print("result = ", sum_symbols("c", "b", "a"))
+print("result = ", sum_numbers(1, 3))
 
 print("result = ", sum_lists([1, 2, 3], [4, 5, 6]))
 print("result = ", sum_lists([1, 2, 2], [4, 1, 6]))
@@ -78,6 +71,7 @@ def func_with_kwargs(title, **kwargs):
 
 
 print(func_with_kwargs('first', a=3, b=4))
+print(func_with_kwargs('first', b=4, a=3))
+print(func_with_kwargs('first', a=100, b=4))
+print(func_with_kwargs('first', a=100, b=4))
 print(func_with_kwargs('first', a=3, b=4))
-print(func_with_kwargs('first', a=100, b=4))  # !!!тоже 7, а должно быть 104
-print(func_with_kwargs('second', a=100, b=4))
